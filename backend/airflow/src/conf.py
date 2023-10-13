226e90c3313b5556df7780694f05deb7e1a3db39
Hugo Sousa <hmosousa@gmail.com>
2023-10-13 16:51:55 +0100

    Added language disclaimer to default answer.

--- backend/airflow/src/conf.py
+++ backend/airflow/src/conf.py
@@ -1,3 +1,3 @@
 """All meta information is mantained here."""
 
-DEFAULT_ANSWER = "I am sorry, I am an AI assistant for physical therapy. I can only help you with physical pain. Here is an example of a question I can answer: 'How can I relieve my back pain?'"
+DEFAULT_ANSWER = "I am sorry, I am an AI assistant for physical therapy. I can only help you with physical disconfort and I am limited to speak in English. Here is an example of a question I can answer: 'How can I relieve my back pain?'"
